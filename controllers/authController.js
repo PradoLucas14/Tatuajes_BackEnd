13389e1e0d40fe15cffbc71f106d32bda44326d6
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-10-19 23:37:49 -0300

    se creo un controlador para la autenticacion de usuarios

---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+// Función para iniciar sesión (login)
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Verificar si el usuario existe
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Usuario no encontrado' });
+    }
+
+    // Comparar la contraseña
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Contraseña incorrecta' });
+    }
+
+    // Generar el token JWT
+    const payload = {
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+      },
+    };
+
+    // Firmar el token con la clave secreta y un tiempo de expiración
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: '1h', // El token expirará en 1 hora
+    });
+
+    // Enviar el token al cliente
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Error en el servidor' });
+  }
+};
