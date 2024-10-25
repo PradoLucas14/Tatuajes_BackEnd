67f62ddbac0006c253695a8279909450aee0fed0
PradoLucas14 <lucasnahuelprado0@gmail.com>
2024-10-25 11:41:24 -0300

    se realizo una modificacion en la estructura de los controladores de reservas para agregar el apartado de hora de la reserva

--- controllers/reservController.js
+++ controllers/reservController.js
@@ -2,13 +2,22 @@ const Reserv = require('../models/Reserv'); // Asumiendo que el modelo se llama 
 
 // Crear una nueva reservación
 const createReserv = async (req, res) => {
-  try {
-    const reserv = new Reserv(req.body);
-    await reserv.save();
-    res.status(201).json(reserv);
-  } catch (error) {
-    res.status(500).json({ error: 'Error al agendar la reservación' });
-  }
+    try {
+      const { cliente, fecha, hora, tatuador } = req.body;
+  
+      // Verificar si ya existe una reservación para el mismo tatuador en la misma fecha y hora
+      const existingReserv = await Reserv.findOne({ fecha, hora, tatuador });
+      if (existingReserv) {
+        return res.status(400).json({ error: 'El tatuador ya tiene una reserva en esta fecha y hora' });
+      }
+  
+      // Crear y guardar la nueva reservación
+      const reserv = new Reserv({ cliente, fecha, hora, tatuador });
+      await reserv.save();
+      res.status(201).json(reserv);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al agendar la reservación' });
+    }
 };
 
 // Obtener todas las reservaciones
