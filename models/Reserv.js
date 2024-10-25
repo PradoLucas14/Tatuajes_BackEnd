ff8751f8419c8987c01e10f86552905dce61fda0
PradoLucas14 <lucasnahuelprado0@gmail.com>
2024-10-25 11:38:20 -0300

    se modifico la estructura del molde de reservas agregando el apartado de horas

--- models/Reserv.js
+++ models/Reserv.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const reservSchema = new mongoose.Schema({
   cliente: { type: String, required: true },
   fecha: { type: Date, required: true },
-  servicio: { type: String, required: true },
+  hora: { type: String, required: true }, // Nuevo campo para la hora
+  tatuador: { type: String, required: true },
   estado: { type: String, enum: ['pendiente', 'completado', 'cancelado'], default: 'pendiente' },
 });
 
