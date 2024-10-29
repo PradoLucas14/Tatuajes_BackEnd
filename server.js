60628938b2230786840bbfc0cc6533ce510ec0b5
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-10-28 23:29:50 -0300

    se agrego el uso de morgan para ver en tiempo real de ejecicion del servidor las peticiones realizadas al mismo

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//server.js
 require('dotenv').config();  // Cargar las variables de entorno desde el archivo .env
 
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const morgan = require('morgan');  // Importar morgan
 const app = express();
 
 // Conectar a la base de datos
@@ -12,12 +12,12 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));  // Agregar morgan como middleware
 
 // Rutas
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/reservs', require('./routes/reservRoutes'));
-
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
